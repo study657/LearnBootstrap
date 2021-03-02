4b1ed44bcba0a4af91cc565beacc6dbcce65b0b6
study657 <79162199605@list.ru>
2021-03-02 13:44:20 +0300

    Сверстали блок с футером, а так же copyrigth блок. Сделали кнопочку, которая при скролинге вниз появляется и при нажатии ведет вверх сайта. Сделали прелодер, который показывает страницу тогда, когда все на ней загруженно

--- Верстка шаблона сайта/js/main.js
+++ Верстка шаблона сайта/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
+	$(window).on('load', function(){
+        $('.preloader').delay(500).fadeOut('slow', function(){
+        	$(this).attr('style', 'display: none !important');
+        });
+    });
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.arrow-up').fadeIn();
+		} else {
+			$('.arrow-up').fadeOut();
+		}
+	});
+
+	$('.arrow-up').click(function(){
+		$('html, body').animate({scrollTop : 0}, 800);
+		return false;
+	});
+
 	baguetteBox.run('.galary');
 });
